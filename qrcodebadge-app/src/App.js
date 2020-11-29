e678b8123bf6db7b463807bfb1281fe2d4bc338c
KrisCampbell <kriscampbell1997@gmail.com>
2020-11-29 14:04:52 -0800

    Added QRCode Badge display & CSS Formatting
    -Added badge display & QRCodge Image
    -Badge text & QRCode info dynamically updated if twitter & github are not empty.
    -Many CSS changes, to buttons and badge display

--- qrcodebadge-app/src/App.js
+++ qrcodebadge-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import './App.css';
+import qrcode from "qrcode-generator";
 
 function App() {
   const [values, setValues] = useState({
@@ -18,21 +19,42 @@ function App() {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    console.log(values.name);
-    console.log(values.email);
-    console.log(values.twitter);
-    console.log(values.gitHub);
+    var typeNumber = 6;
+    var errorCorrectionLevel = 'L';
+    var qr = qrcode(typeNumber, errorCorrectionLevel);
+    var fullString = "Name: " + values.name + "\nEmail: " + values.email;
+
+    // Update badge display, add twitter and github if they exist.
+    var badgeDisplay = '<div class="column"><h2>Name: ' + values.name + '</h2><h2>Email: ' + values.email + '</h2>';
+
+    if (values.twitter !== "") { 
+      badgeDisplay+= '<h2>Twitter: ' + values.twitter + '</h2>';
+      fullString += "\nTwitter: " + values.twitter;
+    }
+    if (values.gitHub !== "") { 
+      badgeDisplay+= '<h2>GitHub: ' + values.gitHub + '</h2>';
+      fullString += "\nGitHub: " + values.gitHub;
+    }
+
+    badgeDisplay += '</div>';
+
+    qr.addData(fullString);
+    qr.make();
+
+    document.getElementById('BadgeContainer_placeHolder').innerHTML = badgeDisplay + '<div class="column">' + qr.createImgTag(5) + '</div>';
   }
 
   const onCancel = () => {
     setValues({
       name: "", email: "", twitter: "", gitHub: ""
     });
+
+    document.getElementById('BadgeContainer_placeHolder').innerHTML = null;
   }
 
   return (
     <div className="App">
-      <h2>QRCode Badge Generator: Kris Campbell</h2>
+      <h1>QRCode Badge Generator: Kris Campbell</h1>
       <div className="InputField">
         <form onSubmit={onSubmit}>
           <input type="text" onChange={handleInput('name')} placeholder="*Name (First and Last)" value={values.name} pattern="^[a-zA-Z,.'-]+ [a-zA-Z,.'-]+$" required />
@@ -43,6 +65,7 @@ function App() {
           <button className="button button2" type="button" onClick={onCancel}>Cancel</button>
         </form>
       </div>
+      <div className="BadgeContainer row" id="BadgeContainer_placeHolder"></div>
     </div>
   );
 }
