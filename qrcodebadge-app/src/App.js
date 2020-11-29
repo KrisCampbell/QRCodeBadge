6aa38088b402c97d768e65326847edfc4fbce0bd
KrisCampbell <kriscampbell1997@gmail.com>
2020-11-29 12:39:11 -0800

    Replace default landing page with basic inputs
    -Remove default React landing page code, add 4 inputs for user's name, email, Twitter, & GitHub.
    -Using regex on inputs to verify first and last name and twitter handle.  Submit button simply logs the values to console for now, cancel button clears values.
    -Simple textfield formatting in App.css file.
    -Imported qrcode-generator into react project, unused for now.

--- qrcodebadge-app/src/App.js
+++ qrcodebadge-app/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    twitter: "",
+    gitHub: ""
+  });
+
+  const handleInput = event => {
+    return ({ target: { value } }) => {
+      setValues(oldValues => ({...oldValues, [event]: value }));
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log(values.name);
+    console.log(values.email);
+    console.log(values.twitter);
+    console.log(values.gitHub);
+  }
+
+  const onCancel = () => {
+    setValues({
+      name: "", email: "", twitter: "", gitHub: ""
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>QRCode Badge Generator: Kris Campbell</h2>
+      <div className="InputField">
+        <form onSubmit={onSubmit}>
+          <input type="text" onChange={handleInput('name')} placeholder="*Name (First and Last)" value={values.name} pattern="^[a-zA-Z,.'-]+ [a-zA-Z,.'-]+$" required />
+          <input type="email" onChange={handleInput('email')} placeholder="*Email" value={values.email} required />
+          <input type="text" onChange={handleInput('twitter')} placeholder="Twitter (@twitterHandle)" pattern="^@[a-zA-Z0-9]+$" value={values.twitter} />
+          <input type="text" onChange={handleInput('gitHub')} placeholder="GitHub" value={values.gitHub} /><br></br>
+          <button className="button button1" type="submit">Create</button>
+          <button className="button button2" type="button" onClick={onCancel}>Cancel</button>
+        </form>
+      </div>
     </div>
   );
 }
